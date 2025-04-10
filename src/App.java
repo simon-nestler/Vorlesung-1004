eb2b249cb7cebf4b8099f76e7759fb38b82a59b7
Simon Nestler <simon.nestler@thi.de>
2025-04-10 11:49:25 +0200

    liste ohne duplikate...

--- src/App.java
+++ src/App.java
@@ -2,14 +2,11 @@ public class App {
     public static void main(String[] args) throws Exception {
         NumberList list = new NumberList();
         list.add(1);
-        list.add(2);
-        list.add(2);
-        list.add(1);
-        list.add(3);
+        list.add(5);
+        list.add(7);
+        list.add(8);
+        list.add(10);
 
         System.out.println(list);
-        System.out.println(list.getFrequency(2));
-        System.out.println(list.getFrequency(1));
-        System.out.println(list.getFrequency(3));
     }
 }
